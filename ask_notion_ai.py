3d7127d8d88563c33e48a429208398cb4f89b70b
Zhike Chen <zk.chen007@gmail.com>
2023-06-10 22:49:06 +0800

    add continue_writing sub command and refactor

--- ask_notion_ai.py
+++ ask_notion_ai.py
@@ -1,43 +1,46 @@
 import click
 import os
 from notionai import NotionAI
-from notionai.enums import ToneEnum, TranslateLanguageEnum
+from notionai.enums import ToneEnum, TranslateLanguageEnum, PromptTypeEnum
 import keyboard
 import pyperclip
 import logging
 
 
-TOKEN = os.getenv("NOTION_TOKEN")
-SPACE_ID = os.getenv("NOTION_SPACE_ID")
-
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
+TOKEN = os.getenv("NOTION_TOKEN")
+SPACE_ID = os.getenv("NOTION_SPACE_ID")
+AI = NotionAI(TOKEN, SPACE_ID)
 
 @click.group()
 @click.option('--copy-input-content', is_flag=True, help='Copy input content to clipboard.')
 @click.option('--keyboard-output', is_flag=True, help='Output result to keyboard instead of stdout.')
+@click.option('--keyboard-output-with-input', is_flag=True, help='make input content as the front part of keyboard output')
 @click.option('--clipboard-output', is_flag=True, help='Copy result to clipboard.')
 @click.option('--verbose', '-v', is_flag=True, help='Enable verbose output.')
-def cli(copy_input_content, keyboard_output, clipboard_output, verbose):
+def cli(copy_input_content, keyboard_output, keyboard_output_with_input, clipboard_output, verbose):
     """Command line interface for Notion AI API."""
-    global COPY_CONTENT, KB_OUTPUT, CLIPBOARD_OUTPUT
+    global COPY_CONTENT, KB_OUTPUT, CLIPBOARD_OUTPUT, KEYBOARD_OUTPUT_WITH_INPUT
     COPY_CONTENT = copy_input_content
     KB_OUTPUT = keyboard_output
     CLIPBOARD_OUTPUT = clipboard_output
+    KEYBOARD_OUTPUT_WITH_INPUT = keyboard_output_with_input
     if verbose:
         LOGGER.level = logging.DEBUG
 
-def copy_content_if_needed(context):
+def output(context, result):
     if COPY_CONTENT:
         pyperclip.copy(context)
-
-def output(result):
     LOGGER.debug(f"notion ai result: {result}")
     if CLIPBOARD_OUTPUT:
         LOGGER.debug("write to clipboard")
         pyperclip.copy(result)
     if KB_OUTPUT:
+        if KEYBOARD_OUTPUT_WITH_INPUT:
+            LOGGER.debug("output with input")
+            result = f"{context}\n{result}"
         LOGGER.debug("write to keyboard")
         keyboard.write(result)
     else:
@@ -49,44 +52,44 @@ def output(result):
 @click.argument('tone')
 @click.argument('context')
 def change_tone(tone, context):
-    ai = NotionAI(TOKEN, SPACE_ID)
     LOGGER.debug(f"change tone to {tone}, input: {context}")
-    copy_content_if_needed(context)
     tone_enum = ToneEnum[tone]
-    resp = ai.change_tone(context, tone_enum)
-    output(resp)
+    resp = AI.change_tone(context, tone_enum)
+    output(context, resp)
 
 @cli.command()
 @click.argument('context')
 def improve_writing(context):
     LOGGER.debug(f"improve writing input: {context}")
-    copy_content_if_needed(context)
-    ai = NotionAI(TOKEN, SPACE_ID)
-    resp = ai.improve_writing(context)
-    output(resp)
+    resp = AI.improve_writing(context)
+    output(context, resp)
+
+@cli.command()
+@click.argument('context')
+def continue_writing(context, page_title=""):
+    LOGGER.debug(f"continue writing input, context:{context}, page_title:{page_title}")
+    resp = AI.writing_with_prompt(PromptTypeEnum.continue_writing, context, page_title)
+    output(context, resp)
 
 @click.command()
 @click.argument('language')
 @click.argument('context')
 def translate(language, context):
-    ai = NotionAI(TOKEN, SPACE_ID)
     LOGGER.debug(f"translate to {language}, input: {context}")
-    copy_content_if_needed(context)
     lang_enum = TranslateLanguageEnum[language]
-    resp = ai.translate(lang_enum, context)
-    output(resp)
+    resp = AI.translate(lang_enum, context)
+    output(context, resp)
 
 @click.command()
 @click.argument('context')
 def summarize(context):
-    ai = NotionAI(TOKEN, SPACE_ID)
     LOGGER.debug(f"summarize input: {context}")
-    copy_content_if_needed(context)
-    resp = ai.summarize(context)
-    output(resp)
+    resp = AI.summarize(context)
+    output(context, resp)
 
 cli.add_command(change_tone)
 cli.add_command(improve_writing)
+cli.add_command(continue_writing)
 cli.add_command(translate)
 cli.add_command(summarize)
 
